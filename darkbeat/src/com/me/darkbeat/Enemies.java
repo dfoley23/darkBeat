4afcfd22bdcb6c13ab7b4ddbff0202db42c41411
Bryan Pacini <wpacini@ucsc.com>
2013-01-25 21:44:59 -0800

    The project

---
+++ darkbeat/src/com/me/darkbeat/Enemies.java
@@ -0,0 +1,28 @@
+package com.me.darkbeat;
+
+public class Enemies{
+	
+	private int position;
+	private int direction;
+	
+	public int getPosition(){
+		return position;
+	}
+	
+	public void setPosition(int newPosition){
+		position = newPosition;
+	}
+	
+	public int getDirection(){
+		return direction;
+	}
+	
+	public void setDirection( int newDirection){
+		direction = newDirection;
+	}
+	
+	public void move(){
+		//TODO
+	}
+	
+}
